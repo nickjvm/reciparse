7508574971448a1efe68d9f5ef3ada40fb814e09
Nick V <nickjvm@gmail.com>
2023-07-13 09:33:40 -0600

    header account style changes

--- src/components/AuthBtn.tsx
+++ src/components/AuthBtn.tsx
@@ -10,6 +10,8 @@ import { HeartIcon, UserIcon, ArrowLeftOnRectangleIcon} from '@heroicons/react/2
 import { useRouter } from 'next/navigation'
 import { AuthAction } from '@/types'
 import { AuthError } from '@supabase/supabase-js'
+import { ChevronDownIcon } from '@heroicons/react/24/solid'
+import classNames from 'classnames'
 
 interface Props {
   onSuccess?: () => void
@@ -111,26 +113,37 @@ function AuthBtn({ onSuccess }: Props) {
       <>
         <div className="hidden whitespace-nowrap md:block">
           <Popover className="relative">
-            <>
-              <Popover.Button className="text-sm font-semibold leading-6 text-gray-900">
-              My Account
-              </Popover.Button>
+            {({ open }) => (
+              <>
+                <Popover.Button className={classNames(
+                  'transition flex items-center text-sm font-semibold leading-6 text-gray-900 hover:bg-slate-50 hover:ring-slate-200 hover:ring-1 focus-visible:bg-slate-50 py-1.5 px-2 -py-1.5 rounded',
+                  open && 'ring-1 bg-slate-50, ring-slate-200'
+                )}>
+                  <span className="sr-only">My Account</span>
+                  <span className="inline-block h-7 w-7 overflow-hidden rounded-full bg-gray-100">
+                    <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                  </span>
+                  <ChevronDownIcon className="w-4 stroke-gray-500 ml-3"/>
+                </Popover.Button>
 
-              <Popover.Panel className="text-sm font-semibold leading-6 shadow absolute mt-2 bg-white right-0 rounded w-40 min-w-fit whitespace-nowrap">
-                <Link href="/account/favorites" className="block hover:bg-slate-100 px-3 py-2">
-                  <HeartIcon className="w-5 inline-block mr-2" />
-                My Favorites
-                </Link>
-                <Link href="/account" className="block hover:bg-slate-100 px-3 py-2">
-                  <UserIcon className="w-5 inline-block mr-2"/>
-                My Profile
-                </Link>
-                <button className="w-full text-left block hover:bg-slate-100 px-3 py-2" type="submit" onClick={handleSignOut}>
-                  <ArrowLeftOnRectangleIcon className="w-5 inline-block mr-2"/>
-                Sign out
-                </button>
-              </Popover.Panel>
-            </>
+                <Popover.Panel className="transition text-sm font-semibold leading-6 shadow absolute mt-2 bg-white right-0 rounded w-40 min-w-fit whitespace-nowrap">
+                  <Link href="/account/favorites" className="block hover:bg-slate-50 px-3 py-2">
+                    <HeartIcon className="w-5 inline-block mr-2" />
+                    My Favorites
+                  </Link>
+                  <Link href="/account" className="transition block hover:bg-slate-50 px-3 py-2">
+                    <UserIcon className="w-5 inline-block mr-2"/>
+                    My Profile
+                  </Link>
+                  <button className="transition w-full text-left block hover:bg-slate-50 px-3 py-2" type="submit" onClick={handleSignOut}>
+                    <ArrowLeftOnRectangleIcon className="w-5 inline-block mr-2"/>
+                    Sign out
+                  </button>
+                </Popover.Panel>
+              </>
+            )}
           </Popover>
         </div>
         <div className="md:hidden space-y-1">
--- src/components/QuickSearch.tsx
+++ src/components/QuickSearch.tsx
@@ -73,7 +73,7 @@ export default function QuickSearch({ size = 'md', inputClassName, autoFocus }: 
             className={classNames(
               error && 'ring-red-300 focus:ring-red-600',
               !error && 'ring-gray-300 focus:ring-brand-alt',
-              'block transition w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 text-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset',
+              'block transition w-full rounded-md border-0 py-1.5 pr-12 md:pr-8 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 text-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset',
               `sm:text-${size}`,
               inputClassName, {
                 'py-3 sm:leading-8 sm:text-lg': size === 'lg',
