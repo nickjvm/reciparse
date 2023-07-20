854eaafb38aa8ae8e5e1857d997d436ac6e64972
Nick V <nickjvm@gmail.com>
2023-07-19 22:20:02 -0600

    fix displaying of ingredient notes/subtext

--- src/components/molecules/Ingredients.tsx
+++ src/components/molecules/Ingredients.tsx
@@ -12,6 +12,23 @@ interface Props {
   showStickyIngredients?: boolean
 }
 
+interface Ingredient {
+  subtext: string
+  primary: string
+}
+const cleanIngredientString = (ingredient: string): Ingredient => {
+  const subtextMatch = ingredient.match(/\s(\(,?\s?(.+))\)$/)
+  let subtext: string|null = null
+
+  if (subtextMatch) {
+    ingredient = ingredient.replace(subtextMatch[0], '').replace(/\s([^\s]+)$/, '&nbsp;$1')
+    subtext = subtextMatch[subtextMatch.length - 1].replace(/\s([^\s]+)$/, '&nbsp;$1')
+  }
+  return {
+    primary: decode(ingredient),
+    subtext: decode(subtext)
+  }
+}
 export default function IngredientsList({ ingredients, showStickyIngredients }: Props) {
   const disclosureRef = useRef<HTMLButtonElement>(null)
   useEffect(() => {
@@ -20,7 +37,7 @@ export default function IngredientsList({ ingredients, showStickyIngredients }: 
         if (disclosureRef.current?.matches('[aria-expanded="true"]')) {
           disclosureRef.current.click()
         }
-      }, 500)
+      }, 150)
     }
   }, [showStickyIngredients])
 
@@ -32,12 +49,15 @@ export default function IngredientsList({ ingredients, showStickyIngredients }: 
           <Copy text={ingredients.join('\n')} />
         </h3>
         <ul className={classNames(ingredients.length > 8 && 'print:columns-2', 'print:mb-4')}>
-          {ingredients.map((ingredient: string, i: number)=> (
-            <li key={i} className="border-b print:border-b-0 print:pb-0 print:mb-0 last:border-b-0 border-b-slate-200 pb-2 mb-2">{decode(ingredient.replace(/\s([^\s]+)$/, '&nbsp;$1'))}</li>
+          {ingredients.map(cleanIngredientString).map((ingredient: Ingredient, i: number) => (
+            <li key={i} className="border-b print:border-b-0 print:pb-0 print:mb-0 last:border-b-0 border-b-slate-200 pb-2 mb-2">
+              {ingredient.primary}
+              {ingredient.subtext && <em className="ml-1.5 text-sm italic text-slate-500">{ingredient.subtext}</em>}
+            </li>
           ))}
         </ul>
       </div>
-      <div className={classNames('block print:hidden md:hidden fixed top-[100%] left-0 right-0 translate-y-0 duration-500 transition-transform', showStickyIngredients && 'z-10 -translate-y-[100%]')}>
+      <div className={classNames('block print:hidden md:hidden fixed top-[100%] left-0 right-0 translate-y-0 transition-transform', showStickyIngredients && 'z-10 -translate-y-[100%]')}>
         <Disclosure>
           {({ open }) => (
             <div className={classNames('block print:hidden md:hidden border-t-2 transition bg-white border-brand')}>
@@ -53,8 +73,11 @@ export default function IngredientsList({ ingredients, showStickyIngredients }: 
               </Disclosure.Button>
               <Disclosure.Panel>
                 <ul className="px-5 overflow-auto max-h-80">
-                  {ingredients.map((ingredient, i: number)=> (
-                    <li key={i} className="border-b last:border-b-0 border-b-slate-200 pb-2 mb-2">{decode(ingredient.replace(/\s([^\s]+)$/, '&nbsp;$1'))}</li>
+                  {ingredients.map(cleanIngredientString).map((ingredient, i: number) => (
+                    <li key={i} className="border-b last:border-b-0 border-b-slate-200 pb-2 mb-2">
+                      {ingredient.primary}
+                      {ingredient.subtext && <em className="ml-1.5 text-sm italic text-slate-500">{ingredient.subtext}</em>}
+                    </li>
                   ))}
                 </ul>
               </Disclosure.Panel>
