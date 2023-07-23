4e7f274a34fc2ccd71c3b040b72a7a0136b7c5a5
Nick V <nickjvm@gmail.com>
2023-07-23 08:52:51 -0600

    enable debug logs for preview envs

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -3,7 +3,7 @@ import env from './getEnv'
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export default function debug(...args: any[]) {
 
-  if (['local', 'development'].includes(env) || global.window?.sessionStorage.getItem('debug')) {
+  if (['local', 'preview', 'development'].includes(env) || global.window?.sessionStorage.getItem('debug')) {
     console.log(...args)
   }
 }
